0ec80351aebafd72691e495aee00b1d3569e1e2e
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-06-27 10:20:53 +0700

    Membuat Object Error Class Sendiri

--- learn-express-middleware/index.js
+++ learn-express-middleware/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 morgan = require("morgan");
 
+const ErrorHandler = require("./ErrorHandler");
+
 app.use(morgan("dev"));
 
 app.use((req, res, next) => {
@@ -16,7 +18,7 @@ const auth = (req, res, next) => {
     next();
   }
   // res.send("Perlu masukkan password");
-  throw new Error("Perlu masukkan password");
+  throw new ErrorHandler("Perlu masukkan password", 401);
 };
 
 app.get("/", (req, res) => {
@@ -36,15 +38,24 @@ app.get("/admin", auth, (req, res) => {
   res.send("Helo admin");
 });
 
-app.use((req, res) => {
-  res.status(404).send("Page not found");
+app.get("/general/error", (req, res) => {
+  throw new ErrorHandler();
 });
 
+// app.use((req, res) => {
+//   res.status(404).send("Page not found");
+// });
+
+// app.use((err, req, res, next) => {
+//   console.log("********************************");
+//   console.log("**************ERROR*************");
+//   console.log("********************************");
+//   next(err);
+// });
+
 app.use((err, req, res, next) => {
-  console.log("********************************");
-  console.log("**************ERROR*************");
-  console.log("********************************");
-  next(err);
+  const { status = 500, message = "Something went wrong" } = err;
+  res.status(status).send(message);
 });
 
 app.listen(3000, () => {
