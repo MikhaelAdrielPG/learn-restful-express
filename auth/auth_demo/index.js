7791bf4e9aa0fc86c15047db5535df2529c43897
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-06-27 22:23:26 +0700

    Implementasi Fungi Login Dengan Bcrypt

--- auth/auth_demo/index.js
+++ auth/auth_demo/index.js
@@ -41,6 +41,25 @@ app.post("/register", async (req, res) => {
   res.redirect("/");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (user) {
+    const isMatch = await bcrypt.compareSync(password, user.password);
+    if (isMatch) {
+      res.redirect("/admin");
+    } else {
+      res.redirect("/login");
+    }
+  } else {
+    res.redirect("/login");
+  }
+});
+
 app.get("/admin", (req, res) => {
   res.send("Halaman admin hanya bisa diakses jika kamu login!");
 });
