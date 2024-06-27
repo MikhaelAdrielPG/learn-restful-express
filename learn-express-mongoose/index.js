1df49766e5d503ba60bfdbd1da6b7b6469c6d0c4
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-06-27 17:04:22 +0700

    implementasi flash session

--- learn-express-mongoose/index.js
+++ learn-express-mongoose/index.js
@@ -2,6 +2,8 @@ const path = require("path");
 const express = require("express");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("connect-flash");
 const app = express();
 const ErrorHandler = require("./ErrorHandler");
 
@@ -23,6 +25,19 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "keyboard-cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.flash_messages = req.flash("flash_messages");
+  next();
+});
 
 function wrapAsync(fn) {
   return function (req, res, next) {
@@ -51,6 +66,7 @@ app.post(
   wrapAsync(async (req, res) => {
     const garment = new Garment(req.body);
     await garment.save();
+    req.flash("flash_messages", "Berhasil menambahkan data Pabrik!");
     res.redirect(`/garments`);
   })
 );
